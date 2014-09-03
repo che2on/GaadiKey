a5aae106cf2ad38b4392bd49de3f07038c71fc59
chethan <che4on@gmail.com>
2014-09-03 14:57:47 +0530

    the body params

--- server.js
+++ server.js
@@ -73,6 +73,12 @@ console.log("This is how the actual req object look like "+req);
 
 console.log("Trying to print the request body "+req.body);
 
+req.body.forEach(dat)
+{
+    console("body param ");
+    console.log(dat);
+}
+
 var thebody = "";
 request.on('data', function(data)
 {
